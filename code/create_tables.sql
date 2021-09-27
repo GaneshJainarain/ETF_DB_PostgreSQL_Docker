54b5c1402b5ff501fd90ed5fc6839d592e8dc3e4
Ganesh Jainarain <RicheyJay2@gmail.com>
2021-09-26 21:55:09 -0400

    finished the stock_price table and added some comments

--- code/create_tables.sql
+++ code/create_tables.sql
@@ -22,10 +22,25 @@ CREATE TABLE etf_holding (
     --The primary key is a combo of the three
     PRIMARY KEY (etf_id, holding_id, dt)
     --This way we have multiple records per holding in the etf for every single day
-
-
+    CONSTRAINT fk_etf FOREIGN KEY (etf_id) REFERENCES stock (id),
+    CONSTRAINT fk_holding FOREIGN KEY (holding_id) REFERENCES stock (id)
+    -- *NOTE* A foreign key is a column (or combination of columns)
+    -- in a table whose values must match values of a column in some other table. 
+    -- FOREIGN KEY constraints enforce referential integrity, which essentially says 
+    -- that if column value A refers to column value B, 
+    -- then column value B must exist.
+    -- In this case when a stock is deleted from the stocks table the data 
+    -- from the holdings and stock price table also delete.
 );
 
 CREATE TABLE stock_price (
-
+    stock_id INTEGER NOT NULL,
+    dt TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    open NUMERIC NOT NULL,
+    high NUMERIC NOT NULL,
+    low NUMERIC NOT NULL,
+    close NUMERIC NOT NULL,
+    volume NUMERIC NOT NULL,
+    PRIMARY KEY(stock_id, dt)
+    CONSTRAINT fk_stock FOREIGN KEY (srock_id) REFERENCES stock (id)
 );
