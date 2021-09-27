fab92a48a5f4d95efb2bbe61d9309ff32d5ff89e
Ganesh Jainarain <RicheyJay2@gmail.com>
2021-09-26 21:39:48 -0400

    finished the etf_holding table, *note* has a compund primary key, based on the db model

--- code/create_tables.sql
+++ code/create_tables.sql
@@ -8,11 +8,21 @@ CREATE TABLE stock (
     name TEXT NOT NULL,
     exchange TEXT NOT NULL,
     is_etf BOOLEAN NOT NULL,
-
-
 );
 
 CREATE TABLE etf_holding (
+    etf_id INTEGER NOT NULL,
+    holding_id INTEGER NOT NULL,
+    --These are just references to a stock,
+    dt DATE NOT NULL,
+    shares NUMERIC,
+    weight NUMERIC,
+    --How do we uniquely identify one of these holdings?
+    --We use a compound primary key
+    --The primary key is a combo of the three
+    PRIMARY KEY (etf_id, holding_id, dt)
+    --This way we have multiple records per holding in the etf for every single day
+
 
 );
 
