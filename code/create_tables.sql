9159f9017785c08c520c71f707d011980cd04380
Ganesh Jainarain <RicheyJay2@gmail.com>
2021-09-26 21:01:47 -0400

    made the stock table and populated it with all the data that it holds via the db model

--- code/create_tables.sql
+++ code/create_tables.sql
@@ -1,5 +1,14 @@
 
 CREATE TABLE stock (
+    id SERIAL PRIMARRY KEY,
+    --Auto incrementing integer 
+    --when we insert a record into this table  itll start with a 1,2,3 etc
+    --easy way to reference a unique record in the stock table 
+    symbol TEXT NOT NULL,
+    name TEXT NOT NULL,
+    exchange TEXT NOT NULL,
+    is_etf BOOLEAN NOT NULL,
+
 
 );
 
